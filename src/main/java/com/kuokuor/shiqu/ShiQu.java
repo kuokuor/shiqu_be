8ae6ae9e02e0ac5d3c2af2a277c30e70cea13a65
kuokuor <2483147040@qq.com>
2022-04-09 14:46:43 +0800

    chore: 构建项目

---
+++ src/main/java/com/kuokuor/shiqu/ShiQu.java
@@ -0,0 +1,11 @@
+package com.kuokuor.shiqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiQu {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiQu.class, args);
+    }
+}
