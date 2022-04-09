a24a55027a97c3e015123bad3626e98c14b01fd6
Great-Biscuit <318215104@qq.com>
2022-04-09 18:17:45 +0800

    feat: 集成Sa-Token并完成测试

--- src/main/java/com/kuokuor/shiqu/controller/UserController.java
+++ src/main/java/com/kuokuor/shiqu/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.kuokuor.shiqu.controller;
 
+import cn.dev33.satoken.annotation.SaCheckLogin;
 import com.kuokuor.shiqu.commom.domain.R;
 import com.kuokuor.shiqu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import cn.dev33.satoken.stp.StpUtil;
 
 /**
  * 用户Controller
@@ -18,9 +20,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @SaCheckLogin
     @RequestMapping("/getUser")
     public R getUserById(int id) {
         return R.ok(userService.selectUserById(id));
+    }
+
+    @SaCheckLogin
+    @RequestMapping("/getHolderUserId")
+    public R getHolderUserId() {
+        return R.ok(StpUtil.getLoginIdAsInt());
     }
 
 }
